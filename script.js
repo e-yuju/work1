d28ff685b440b792bafd1bb8ccaab70f72716ca5
Lee Yuju <83266103+e-yuju@users.noreply.github.com>
2021-09-13 12:43:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){ 
+    $('#toggle1').click(function() {
+        debugger;
+      $('#imgs1').fadeToggle(1000);
+  });
+    $('#toggle2').click(function() {
+        debugger;
+      $('#imgs2').fadeToggle(1000);
+  });
+    $('#toggle3').click(function() {
+        debugger;
+      $('#imgs3').fadeToggle(1000);
+  });
+ });
+
